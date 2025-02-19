afc452e577d3363c2eff8b9ac25f1cb6c5f072ee
breziperzup <danielyung144@outlook.com>
2025-02-19 16:06:55 +1100

    fixed linting

--- aussie-tutor/src/app/cards/[cardname]/page.tsx
+++ aussie-tutor/src/app/cards/[cardname]/page.tsx
@@ -118,7 +118,7 @@ export default function Card() {
           <div className="flex flex-wrap flex-row justify-center w-2/3 gap-4">
           {isSearching && <LoadingWheel/>}
           {(!isSearching && data.length === 0) && 
-          <p className="mt-20 italic text-gray-400">Looks like this card is playing hard to get. It's out of stock for now!</p>}
+          <p className="mt-20 italic text-gray-400">Looks like this card is playing hard to get. It&apos;s out of stock for now!</p>}
             {data.map((card: CardDetails, index) => (
               <InfoPanel key={index} card={card} />
             ))}
